735170cc44eb46ecaca7b349ef67d35533e11b9f
Wenguo Gao <wenguo.gao@outlook.com>
2021-11-22 16:43:56 -0500

    #change1 112221

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,7 @@
-dd
+## download the data file
+if(!file.exists("./data")) {dir.create("./data")}
+Url = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(Url, destfile = "./data/data.zip", method = "curl")
+unzip("./data/data.zip")
+
+test_data = read.table("./UCI HAR Dataset/test/X_test.txt")
